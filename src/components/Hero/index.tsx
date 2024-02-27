cba2bde41d233f0ce229ebfdecd7ff48a93561bf
Malinda Jayawardana <malindajayawardana4@gmail.com>
2024-02-27 18:29:48 +0530

    Update max-width in Hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
-              <div className="mx-auto max-w-[800px] text-center">
+              <div className="mx-auto max-w-[1200px] text-center">
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   46350 software engineer jobs
                 </h1>
